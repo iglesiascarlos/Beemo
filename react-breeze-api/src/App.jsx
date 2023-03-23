a759aab268e583b9e92612cf642f0babfe223699
Carlos Iglesias <iglesiasscarlos@gmail.com>
2023-03-23 18:54:38 +0100

    archivos .env

--- react-breeze-api/src/App.jsx
+++ react-breeze-api/src/App.jsx
@@ -3,6 +3,7 @@ import LogIn from "./components/LogIn";
 import Register from "./components/Register";
 import Profile from "./components/Profile";
 import Forgot from "./components/Forgot";
+import Notfound from "./components/NotFound";
 
 import { Routes, Route } from "react-router-dom"
 
@@ -16,6 +17,7 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/user" element={<Profile />} />
           <Route path="/forgot-password" element={<Forgot />}/>
+          <Route path="*" element={<Notfound />}/>
       </Routes>
     </div>
   )
